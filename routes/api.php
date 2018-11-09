0528c627cab008d7e83615e624cd8d96308a9cf3
samlinncon <samlinncon@gmail.com>
2018-11-09 15:20:54 +0300

    settings package to manage the token generated

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/generate-token','HooksController@generateToken');
+
+Route::post('/account/balance','HooksController@checkAccountBalance');
